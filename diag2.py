ff3ebff6dcda7f7fa5e19d323b3f8c5acac7a5ee
siri chandana <siric@MacBook-Air.local>
2026-09-23 22:28:41 -0400

    Remove debugging scratch files

--- diag2.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-with sync_playwright() as pw:
- browser = pw.chromium.launch(headless=True)
- page = browser.new_page()
- page.goto("http://localhost:5000/")
- print("TITLE:", repr(page.title()))
- print("CONTENT LENGTH:", len(page.content()))
- print("FIRST 300 CHARS:", page.content()[:300])
- browser.close()
